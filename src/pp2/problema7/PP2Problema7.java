72718005e2a2e04c903f052617684acc0f1d7bbd
PC <PC@HP>
2016-10-11 13:39:45 -0500

    7.	Realiza un programa que convierta un valor expresado en Km/h a m/s.

--- src/pp2/problema7/PP2Problema7.java
+++ src/pp2/problema7/PP2Problema7.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package pp2.problema7;
-
+import java.util.*;
 /**
  *
  * @author PC
@@ -15,7 +15,20 @@ public class PP2Problema7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        muestraResultado(convertirCantidad());
     }
-    
+    public static double solicitarDatos(String d){ //Solicita datos
+        double miDato;
+        Scanner entradaEscaner = new Scanner(System.in);
+        System.out.print("Introduce el dato " + d);
+        miDato= entradaEscaner.nextDouble();
+        return miDato;
+    }
+    public static double convertirCantidad(){ 
+        double m = solicitarDatos("kilometros por hora");//Se solicitan los kilometros por hora
+        return (m*1000)/3600; //Los metros por 1000 entre 3600
+    }
+    public static void muestraResultado(double d){
+        System.out.print("El resultado en metros sobre segundos es " + d); //Muestra resultado
+    }
 }
